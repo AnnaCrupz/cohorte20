5c38cfac3a03fe99dd2e250edddee57a76deb415
Ana Karen Escobar Cruz <101671249+AnnaCrupz@users.noreply.github.com>
2022-12-19 17:27:24 -0600

    Ingreso spring boot security

--- Java web/java web-1-nuevo/ejemplo0-4jwt/src/main/java/com/example/demo/myappdbjwt/controllers/PruebaController.java
+++ Java web/java web-1-nuevo/ejemplo0-4jwt/src/main/java/com/example/demo/myappdbjwt/controllers/PruebaController.java
@@ -1,19 +1,55 @@
 package com.example.demo.myappdbjwt.controllers;
 
+import java.security.Security;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import ch.qos.logback.classic.Logger;
+
+
 
 @RestController
 @RequestMapping("")
 public class PruebaController {
+	private static final Logger logger =(Logger)LoggerFactory.getLogger(PruebaController.class);
+	
+	
 @GetMapping("/mensaje")
   public ResponseEntity<?> getMensaje(){
+	var auth= SecurityContextHolder.getContext().getAuthentication();
+	logger.info("Datos usuario {}",auth.getPrincipal());
+	logger.info("permisos usuario", auth.getAuthorities());
+	logger.info("estado usuario", auth.isAuthenticated());
 	Map<String, String> mensaje=new HashMap<>();
 	
 	mensaje.put("contenido","Hola Mundo");
+	return ResponseEntity.ok(mensaje);
+}
+@GetMapping("/publico")
+public ResponseEntity<?> getMensajePublico(){
+	var auth= SecurityContextHolder.getContext().getAuthentication();
+	logger.info("Datos usuario {}",auth.getPrincipal());
+	logger.info("permisos usuario", auth.getAuthorities());
+	logger.info("estado usuario", auth.isAuthenticated());
+	Map<String, String> mensaje=new HashMap<>();
+	
+	mensaje.put("contenido","Hola mi querido publico JWT");
+	return ResponseEntity.ok(mensaje);
+}
+@GetMapping("/admin")
+public ResponseEntity<?> getMensajeAdmin(){
+	var auth= SecurityContextHolder.getContext().getAuthentication();
+	logger.info("Datos usuario {}",auth.getPrincipal());
+	logger.info("permisos usuario", auth.getAuthorities());
+	logger.info("estado usuario", auth.isAuthenticated());
+	Map<String, String> mensaje=new HashMap<>();
+	
+	mensaje.put("contenido","Hola mi querido admin JWT");
 	return ResponseEntity.ok(mensaje);
 }
 }
